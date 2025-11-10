7b9a141b200b4cc33ad44de1a225577c4f0d9638
Gokul Nagesh <gokul@jivrus.com>
2025-11-10 23:50:04 +0530

    Day 1 - Get Started - Dynamic Date time Agent - Time Assistant

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -1,8 +1,17 @@
 from google.adk.agents.llm_agent import Agent
+from datetime import datetime
+
+
+def get_instructions() -> str:
+    """Returns the instructions."""
+    current_time = datetime.now()
+    instruction = f"My current time is {current_time} in India, please calculate the time with respect to the timezones for the given city. Please Provide the answer in the format '12th December 2025 HH:MM AM/PM."
+    return instruction
+
 
 root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    description="Tells the current time in a specified city.",
+    instruction=get_instructions(),  # call the function to pass a string
 )
